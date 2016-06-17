68c63ccbe8ea2afaf7ec71479611e7ee27ec66bc
Loui V <loui@betabrand.com>
2016-06-17 15:07:00 -0700

    First commit for site folder.

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="Scotch">
+
+<title>Scoria</title>
+
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" type="text/css" href="css/app.css">
---
+++ resources/views/pages/projects.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+    i am the projects page
+@stop
